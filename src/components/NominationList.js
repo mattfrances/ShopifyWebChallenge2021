a7cad29f48797dcef9ab936571e4578d73a99454
mattfrances <matfran23@gmail.com>
2021-01-15 08:51:40 -0500

    improved share button styling

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -18,13 +18,20 @@ const NominationList = ({ nominated, removeNomination }) => {
 
   return (
     <div className="w-full md:w-1/2">
-      <h2 className="text-lg font-bold dark:text-white">
-        My nominations
-        <span className="ml-1.5 text-sm text-gray-400 font-normal">{`${
-          5 - nominated.length
-        } remaining`}</span>
-      </h2>
-      <button onClick={() => handleShareNominations()}>Share</button>
+      <div className="flex justify-between">
+        <h2 className="text-lg font-bold dark:text-white">
+          My nominations
+          <span className="ml-1.5 text-sm text-gray-400 font-normal">{`${
+            5 - nominated.length
+          } remaining`}</span>
+        </h2>
+        <button
+          className="rounded px-1.5 py-0.5 text-blue-600 hover:text-blue-500 focus:outline-none"
+          onClick={() => handleShareNominations()}
+        >
+          Share
+        </button>
+      </div>
       {nominated.length === 0 && (
         <MovieEmptyState message={'Nominate a movie to see it here.'} />
       )}
